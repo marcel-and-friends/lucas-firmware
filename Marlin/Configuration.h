ce5c9366451b51a8eee0611694b854ae2959de97
vinicius <andrade.vinicius934@gmail.com>
2023-03-29 10:16:14 -0300

    alterar driver do 'E0' para o correto

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -877,7 +877,7 @@
 //#define J_DRIVER_TYPE  A4988
 //#define K_DRIVER_TYPE  A4988
 #define E0_DRIVER_TYPE TMC2208 //Wilson
-#define E1_DRIVER_TYPE TMC2209 //Wilson
+#define E1_DRIVER_TYPE TMC2208 //Wilson
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
 //#define E4_DRIVER_TYPE A4988
