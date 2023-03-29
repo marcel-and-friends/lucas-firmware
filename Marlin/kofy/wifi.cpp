56f05c8b67ae4a0ff59e4e17336e45b42bb62a90
vinicius <andrade.vinicius934@gmail.com>
2023-03-29 10:17:26 -0300

    pequenos ajustes no código de wifi

--- Marlin/kofy/wifi.cpp
+++ Marlin/kofy/wifi.cpp
@@ -60,7 +60,7 @@ static void enviar_protocolo(Mensagem tipo, It&& it) {
 	* | Segmento | Tamanho (bytes) | Descrição                                                          |
 	* +----------+-----------------+--------------------------------------------------------------------+
 	* | Início   | 1 (uint8_t)     | Início do protocolo - sempre 0xA5                                  |
-	* | Tipo     | 1 (uint8_t)     | Tipo da mensagem - veja o enum 'Tipo'                              |
+	* | Tipo     | 1 (uint8_t)     | Tipo da mensagem - veja o enum 'Mensagem'                          |
 	* | Tamanho  | 2 (uint16_t)    | Tamanho da mensagem - máximo de 1024 - 5 (bytes reservados) = 1019 |
 	* | Mensagem | Tamanho         | Mensagem em sí                                                     |
 	* | Fim      | 1 (uint8_t)     | Fim do protocolo - sempre 0xFC                                     |
@@ -70,7 +70,7 @@ static void enviar_protocolo(Mensagem tipo, It&& it) {
 	*/
 	constexpr byte INICIO = 0xA5;
 	constexpr byte FIM = 0xFC;
-	constexpr auto BYTES_RESERVADOS = 5;
+	constexpr auto BYTES_RESERVADOS = 5; // u8 + u8 + u16 + u8
 
 	uint16_t tamanho_msg = std::distance(it.begin(), it.end());
 
@@ -80,8 +80,8 @@ static void enviar_protocolo(Mensagem tipo, It&& it) {
 	buffer.push_back(INICIO);
 	// Tipo
 	buffer.push_back(static_cast<byte>(tipo));
-	// Tamanho (16 bytes dividídos em 2)
-	buffer.push_back(tamanho_msg & 0xFF);
+	// Tamanho (16 bytes divididos em 2)
+	buffer.push_back(tamanho_msg & 0xFF); 
 	buffer.push_back((tamanho_msg >> 8) & 0xFF);
 	// Mensagem
 	buffer.insert(buffer.end(), it.begin(), it.end());
@@ -93,7 +93,7 @@ static void enviar_protocolo(Mensagem tipo, It&& it) {
 
 template<typename... Bytes>
 static void enviar_protocolo(Mensagem tipo, Bytes... bytes) {
-	std::array<byte, sizeof...(Bytes)> msg = { static_cast<byte>(bytes)... };
+	std::array msg = { static_cast<byte>(bytes)... };
 	enviar_protocolo(tipo, msg);
 }
 
@@ -101,20 +101,20 @@ void conectar(std::string_view nome_rede, std::string_view senha_rede) {
 	// u8 + u8 + nome_rede + u8 + senha_rede
 	auto tamanho_msg = 1 + 1 + nome_rede.size() + 1 + senha_rede.size();
 
-	std::vector<byte> msg;
-	msg.reserve(tamanho_msg);
+	std::vector<byte> config_msg;
+	config_msg.reserve(tamanho_msg);
 	// Modo
-	msg.push_back(static_cast<byte>(ModoConexao::Cliente));
+	config_msg.push_back(static_cast<byte>(ModoConexao::Cliente));
 	// Tamanho do nome da rede
-	msg.push_back(nome_rede.size());
+	config_msg.push_back(nome_rede.size());
 	// Nome da rede
-	msg.insert(msg.end(), nome_rede.begin(), nome_rede.end());
+	config_msg.insert(config_msg.end(), nome_rede.begin(), nome_rede.end());
 	// Tamanho da senha da rede
-	msg.push_back(senha_rede.size());
+	config_msg.push_back(senha_rede.size());
 	// Senha da rede
-	msg.insert(msg.end(), senha_rede.begin(), senha_rede.end());
+	config_msg.insert(config_msg.end(), senha_rede.begin(), senha_rede.end());
 
-	enviar_protocolo(Mensagem::Config, std::move(msg));
+	enviar_protocolo(Mensagem::Config, config_msg);
 	enviar_protocolo(Mensagem::Conectar, Operacao::Conectar);
 
 	DBG("conectando wifi...");
