49654bb0d863c3989f108334559118606319a007
vinicius <andrade.vinicius934@gmail.com>
2024-02-21 16:11:24 -0300

    cfg: bump version :)

--- Marlin/lucas/cfg/cfg.h
+++ Marlin/lucas/cfg/cfg.h
@@ -37,7 +37,7 @@ enum Options {
 
 using OptionList = std::array<Option, usize(Options::Count)>;
 
-constexpr auto FIRMWARE_VERSION = "1.0.43";
+constexpr auto FIRMWARE_VERSION = "1.0.57";
 
 void setup();
 
